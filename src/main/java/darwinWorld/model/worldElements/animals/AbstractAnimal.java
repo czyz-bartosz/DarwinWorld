9157386d263518ee6830363ed5bb49933569b4b9
Bartosz Czyż <czyzbartosz3@gmail.com>
2024-12-23 19:07:54 +0100

    add WorldMap

--- src/main/java/darwinWorld/model/worldElements/animals/AbstractAnimal.java
+++ src/main/java/darwinWorld/model/worldElements/animals/AbstractAnimal.java
@@ -2,6 +2,7 @@ package darwinWorld.model.worldElements.animals;
 
 import darwinWorld.enums.MoveRotation;
 import darwinWorld.model.Vector2d;
+import darwinWorld.model.WorldMap;
 import darwinWorld.model.worldElements.IWorldElement;
 import darwinWorld.model.worldElements.animals.geneSelectionStrategies.IGeneSelectionStrategy;
 
@@ -9,7 +10,7 @@ import java.util.List;
 
 public abstract class AbstractAnimal implements IWorldElement {
     private Vector2d position;
-    private final MoveRotation rotation;
+    private MoveRotation rotation;
     private final List<MoveRotation> genes;
     private int geneCurrentIndex;
 
@@ -30,15 +31,27 @@ public abstract class AbstractAnimal implements IWorldElement {
         return position;
     }
 
+    public void setPosition(Vector2d position) {
+        this.position = position;
+    }
+
     public MoveRotation getRotation() {
         return rotation;
     }
 
-    public void move(IGeneSelectionStrategy strategy) {
-        geneCurrentIndex = strategy.selectNextGene(genes, geneCurrentIndex);
+    public void setRotation(MoveRotation rotation) {
+        this.rotation = rotation;
+    }
+
+    public void move(
+            IGeneSelectionStrategy geneSelectionStrategy,
+            WorldMap worldMap
+    ) {
+        geneCurrentIndex = geneSelectionStrategy.selectNextGene(genes, geneCurrentIndex);
         MoveRotation currentRotation = MoveRotation.add(rotation, genes.get(geneCurrentIndex));
-        // TODO: add move validation
-        position = position.add(currentRotation.toVector());
+        Vector2d newPosition = position.add(currentRotation.toVector());
+        position = worldMap.getPosition(newPosition);
+        rotation = worldMap.getRotation(newPosition);
     }
 
     public abstract void eat(int energyFromFood);
