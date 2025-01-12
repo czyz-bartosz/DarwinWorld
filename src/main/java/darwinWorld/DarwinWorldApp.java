4ca34eb8deb2041b3f04de681f28c32349381deb
Bartosz Czyż <czyzbartosz3@gmail.com>
2025-01-12 17:08:21 +0100

    feat: init javfx

---
+++ src/main/java/darwinWorld/DarwinWorldApp.java
@@ -0,0 +1,11 @@
+package darwinWorld;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DarwinWorldApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/darwinWorld/MainGUI.java
@@ -0,0 +1,9 @@
+package darwinWorld;
+
+import javafx.application.Application;
+
+public class MainGUI {
+    public static void main(String[] args) {
+        Application.launch(DarwinWorldApp.class, args);
+    }
+}
